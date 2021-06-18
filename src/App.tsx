7171978e72fd25163ddf05df87e303d8d3d4340d
anesmeister <medjamia.anes@gmail.com>
2021-06-18 14:12:01 +0100

    Added application router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Main from "./views/Main";
+import SearchResult from "./views/SearchResult";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="app-container">
+    <Router>
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/search/:query" component={SearchResult} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
