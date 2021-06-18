9ee20940d99556061c49d2bfbe5e93e8790bbc64
anesmeister <medjamia.anes@gmail.com>
2021-06-18 14:09:31 +0100

    Created Main screen

---
+++ src/views/Main/Main.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import AppLogo from "../../components/AppLogo";
+import SearchInput from "../../components/SearchInput";
+import { useHistory } from "react-router-dom";
+import "./Main.css";
+
+const Main: React.FC = () => {
+  const history = useHistory();
+  const [search, setSearch] = React.useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearch(e.target.value);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!search.trim()) return;
+    history.push(`/search/${search}`);
+  };
+
+  return (
+    <div className="main">
+      <AppLogo />
+      <form onSubmit={handleSubmit}>
+        <SearchInput
+          placeHolder="Search for your show ..."
+          handleOnChange={handleChange}
+          containerStyles={{ marginTop: 32 }}
+          value={search}
+          onClear={() => setSearch("")}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Main;
